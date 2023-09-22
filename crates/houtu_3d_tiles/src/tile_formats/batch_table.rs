b5957b6f32c859683ddb46611a133bf8e9c7b486
ZoOL <zhooul@gmail.com>
2023-09-22 19:02:57 +0800

    hoevo_3d_tiles:  update BatchTable test

--- crates/houtu_3d_tiles/src/tile_formats/batch_table.rs
+++ crates/houtu_3d_tiles/src/tile_formats/batch_table.rs
@@ -1,6 +1,21 @@
+use std::collections::HashMap;
+
 use serde::{Deserialize, Serialize};
+use strum_macros::EnumString;
 
 use crate::tile_formats::feature_table::ComponentType;
+
+/// A set of properties defining application-specific metadata for features in a tile.
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct BatchTable {
+    /// An object defining the reference to a section of the binary body of the batch table where the property values are stored if not defined directly in the JSON.
+    pub binary_body_reference: Option<BinaryBodyReference>,
+    /// A user-defined property which specifies per-feature application-specific metadata in a tile. Values either can be defined directly in the JSON as an array, or can refer to sections in the binary body with a `BinaryBodyReference` object.
+    pub property: Option<Property>,
+    #[serde(flatten)]
+    pub additional_properties: HashMap<String, Property>,
+}
 
 /// An object defining the reference to a section of the binary body of the batch table where the property values are stored if not defined directly in the JSON.
 #[derive(Debug, Serialize, Deserialize, PartialEq)]
@@ -10,114 +25,62 @@ pub struct BinaryBodyReference {
     /// The datatype of components in the property.
     pub component_type: ComponentType,
     #[serde(rename = "type")]
-    pub type_: ScaleOrVectorType,
+    pub type_: BinaryBodyReferenceType,
 }
 
-/// A set of properties defining application-specific metadata for features in a tile.
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct BatchTable {
-    /// An object defining the reference to a section of the binary body of the batch table where the property values are stored if not defined directly in the JSON.
-    pub binary_body_reference: Option<BinaryBodyReference>,
-    /// A user-defined property which specifies per-feature application-specific metadata in a tile. Values either can be defined directly in the JSON as an array, or can refer to sections in the binary body with a `BinaryBodyReference` object.
-    pub property: Option<PropertySchema>,
-}
-
-#[derive(Debug, Serialize, PartialEq)]
-pub enum PropertySchema {
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+#[serde(untagged)]
+pub enum Property {
     BinaryBodyReference(BinaryBodyReference),
     Array(Vec<serde_json::Value>),
 }
 
-impl<'de> serde::Deserialize<'de> for PropertySchema {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: serde::Deserializer<'de>,
-    {
-        let value = serde_json::Value::deserialize(deserializer)?;
-        match value {
-            serde_json::Value::Array(array) => Ok(PropertySchema::Array(array)),
-            serde_json::Value::Object(object) => {
-                let byte_offset = object.get("byteOffset");
-                let component_type = object.get("componentType");
-                let type_ = object.get("type");
-
-                if let (Some(byte_offset), Some(component_type), Some(type_)) =
-                    (byte_offset, component_type, type_)
-                {
-                    let byte_offset = byte_offset.as_u64().unwrap();
-                    let component_type = serde_json::from_str(component_type.to_string().as_str());
-                    let type_ = serde_json::from_str(type_.to_string().as_str());
-                    if component_type.is_err() || type_.is_err() {
-                        return Err(serde::de::Error::custom(
-                            "byteOffset, componentType, type must be defined",
-                        ));
-                    }
-                    let component_type = component_type.unwrap();
-                    let type_ = type_.unwrap();
-
-                    Ok(PropertySchema::BinaryBodyReference(BinaryBodyReference {
-                        byte_offset,
-                        component_type,
-                        type_,
-                    }))
-                } else {
-                    Err(serde::de::Error::custom(
-                        "byteOffset, componentType, type must be defined",
-                    ))
-                }
-            }
-            _ => Err(serde::de::Error::custom(
-                "byteOffset, componentType, type must be defined",
-            )),
-        }
-    }
-}
-
 /// Specifies if the property is a scalar or vector.
-#[derive(Debug, PartialEq)]
-pub enum ScaleOrVectorType {
+#[derive(Debug, Serialize, Deserialize, PartialEq, EnumString)]
+pub enum BinaryBodyReferenceType {
     SCALAR,
     VEC2,
     VEC3,
     VEC4,
-    Other(String),
-}
-
-impl<'de> serde::Deserialize<'de> for ScaleOrVectorType {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: serde::Deserializer<'de>,
-    {
-        let value = String::deserialize(deserializer)?;
-        match value.as_str() {
-            "SCALAR" => Ok(ScaleOrVectorType::SCALAR),
-            "VEC2" => Ok(ScaleOrVectorType::VEC2),
-            "VEC3" => Ok(ScaleOrVectorType::VEC3),
-            "VEC4" => Ok(ScaleOrVectorType::VEC4),
-            _ => Ok(ScaleOrVectorType::Other(value)),
-        }
-    }
-}
-
-impl serde::Serialize for ScaleOrVectorType {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: serde::Serializer,
-    {
-        match self {
-            ScaleOrVectorType::SCALAR => serializer.serialize_str("SCALAR"),
-            ScaleOrVectorType::VEC2 => serializer.serialize_str("VEC2"),
-            ScaleOrVectorType::VEC3 => serializer.serialize_str("VEC3"),
-            ScaleOrVectorType::VEC4 => serializer.serialize_str("VEC4"),
-            ScaleOrVectorType::Other(value) => serializer.serialize_str(value),
-        }
-    }
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
+    use serde_json::json;
+
+    #[test]
+    fn test_property() {
+        let json = json!([1, 2, 3]);
+        let property: Property = serde_json::from_value(json).unwrap();
+
+        assert_eq!(
+            property,
+            Property::Array(vec![json!(1), json!(2), json!(3)])
+        );
+
+        let json = json!({
+            "byteOffset": 0,
+            "componentType": "UNSIGNED_SHORT",
+            "type": "SCALAR"
+        });
+        let property: Property = serde_json::from_value(json).unwrap();
+        let binary_body_reference: BinaryBodyReference =
+            if let Property::BinaryBodyReference(binary_body_reference) = property {
+                binary_body_reference
+            } else {
+                panic!("property must be binary body reference")
+            };
+
+        assert_eq!(
+            binary_body_reference,
+            BinaryBodyReference {
+                byte_offset: 0,
+                component_type: ComponentType::UNSIGNED_SHORT,
+                type_: BinaryBodyReferenceType::SCALAR,
+            }
+        );
+    }
 
     #[test]
     fn test_batch_table() {
@@ -136,18 +99,13 @@ mod test {
                 },
                     {
                     "byteOffset": 1,
-                    "componentType": "OTHER",
-                    "type": "OTHER"
+                    "componentType": "DOUBLE",
+                    "type": "VEC3"
                 },
                 {
                     "byteOffset": 2,
                     "componentType": "UNSIGNED_SHORT",
                     "type": "VEC3"
-                },
-                {
-                    "byteOffset": 3,
-                    "componentType": "UNSIGNED_SHORT",
-                    "type": "OTHER"
                 }
             ]
         }
@@ -158,7 +116,7 @@ mod test {
         let binary_body_reference: BinaryBodyReference = batch_table.binary_body_reference.unwrap();
         assert!(batch_table.property.is_some());
 
-        let property = if let PropertySchema::Array(property) = batch_table.property.unwrap() {
+        let property = if let Property::Array(property) = batch_table.property.unwrap() {
             property
         } else {
             panic!("property must be array")
@@ -168,15 +126,14 @@ mod test {
             ComponentType::UNSIGNED_SHORT
         );
         assert_eq!(binary_body_reference.byte_offset, 0);
-        assert_eq!(binary_body_reference.type_, ScaleOrVectorType::SCALAR);
+        assert_eq!(binary_body_reference.type_, BinaryBodyReferenceType::SCALAR);
         let p0: BinaryBodyReference = serde_json::from_value(property[0].clone()).unwrap();
         let p1: BinaryBodyReference = serde_json::from_value(property[1].clone()).unwrap();
         let p2: BinaryBodyReference = serde_json::from_value(property[2].clone()).unwrap();
-        let p3: BinaryBodyReference = serde_json::from_value(property[3].clone()).unwrap();
         assert_eq!(p0.component_type, ComponentType::UNSIGNED_SHORT);
+        assert_eq!(p1.component_type, ComponentType::DOUBLE);
         assert_eq!(p2.component_type, ComponentType::UNSIGNED_SHORT);
-        assert_eq!(p2.type_, ScaleOrVectorType::VEC3);
-        assert_eq!(p3.type_, ScaleOrVectorType::Other("OTHER".to_string()));
+        assert_eq!(p2.type_, BinaryBodyReferenceType::VEC3);
 
         let json = r#"
         {
@@ -195,10 +152,10 @@ mod test {
             BinaryBodyReference {
                 byte_offset: 0,
                 component_type: ComponentType::UNSIGNED_SHORT,
-                type_: ScaleOrVectorType::SCALAR,
+                type_: BinaryBodyReferenceType::SCALAR,
             }
         );
-        assert_eq!(property, Some(PropertySchema::Array(vec![])));
+        assert_eq!(property, Some(Property::Array(vec![])));
 
         let json = r#"
         {
@@ -217,12 +174,12 @@ mod test {
         }"#;
         let batch_table: BatchTable = serde_json::from_str(json).unwrap();
         let property =
-            if let PropertySchema::BinaryBodyReference(property) = batch_table.property.unwrap() {
+            if let Property::BinaryBodyReference(property) = batch_table.property.unwrap() {
                 property
             } else {
                 panic!("property must be array")
             };
-        assert_eq!(property.type_, ScaleOrVectorType::SCALAR);
+        assert_eq!(property.type_, BinaryBodyReferenceType::SCALAR);
         assert_eq!(property.component_type, ComponentType::UNSIGNED_SHORT);
         assert_eq!(property.byte_offset, 0);
     }
